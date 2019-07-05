51235793cf7b46b4393159ddff6d69e952f82856
Firman Hidayat <hideyatsuapp@gmail.com>
2019-07-05 23:29:53 +0700

    add application url

--- config/bpjs.php
+++ config/bpjs.php
@@ -6,4 +6,5 @@ return [
     'username'   => env('BPJS_USERNAME', 'username'),
     'password'   => env('BPJS_PASSWORD', 'password'),
     'app_code'   => env('BPJS_APP_CODE', '095'), // kode aplikasi
+    'app_url'    => env('BPJS_APP_URL', 'https://dvlp.bpjs-kesehatan.go.id:9081/pcare-rest-v3.0/'), // url aplikasi
 ]
